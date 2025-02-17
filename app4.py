772dbf2ac71cd3a98fdb49ab357ef2f120d91a2b
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-17 09:49:13 -0600

    Update app4.py

--- app4.py
+++ app4.py
@@ -12,6 +12,25 @@ import plotly.express as px
 import os
 from pathlib import Path
 
+
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 def simulate_pension(ahorro_inicial, aportacion_mensual, rendimiento_anual, inflacion_anual, años, estrategia, crisis):
     saldo = ahorro_inicial
     data = []
